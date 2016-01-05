7f5b989c097187220669efbaad57676a9a96331a
Tony Young <ty_training@hotmail.co.uk>
2016-01-05 14:00:00 +0000

    Tests for returning 'fizz', 'buzz' and 'fizzbuzz' and logic implemented

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -39,4 +39,28 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByFifteen(20)).toBe(false);
     });
   });
+
+  describe('when playing, says', function() {
+    it('"Fizz" if number is divisible by 3', function() {
+      expect(fizzbuzz.says(3)).toEqual("Fizz");
+    });
+  });
+
+  describe('when playing, says', function() {
+    it('"Buzz" ifnumber is divisible by 5', function() {
+      expect(fizzbuzz.says(5)).toEqual("Buzz");
+    });
+  });
+
+  describe('when playing, says', function() {
+    it('"FizzBuzz" if number is divisible by 15', function() {
+      expect(fizzbuzz.says(15)).toEqual("FizzBuzz");
+    });
+  });
+
+  describe('when playing, says', function() {
+    it('number if not divisible by 3, 5 or 15', function() {
+      expect(fizzbuzz.says(7)).toEqual(7);
+    });
+  });
 });
